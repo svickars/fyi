b18758d1395c7a84041ffddedcf87f0299a1f317
svickars <sam@thedataface.com>
2021-07-24 11:42:55 -0700

    update github pages

--- src/components/blocks/Pic.svelte
+++ src/components/blocks/Pic.svelte
@@ -11,7 +11,7 @@
 <div class="relative {width} animate-pop-delay-{i} opacity-0">
   <div
     class="w-full transition transform bg-center bg-cover rounded shadow-{shadow} filter hover:animate-pop-hover"
-    style="background-image: url(assets/media/{src}); height:{clientWidth}px"
+    style="background-image: url(assets/media{src}); height:{clientWidth}px"
     {title}
   />
   <div class="absolute top-0 left-0 w-full" bind:clientWidth />
